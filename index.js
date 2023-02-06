275d9f1ed60920e38b4be84f6590854b0343e09e
danila9090 <124569828+danila9090@users.noreply.github.com>
2023-02-06 17:37:55 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,117 @@
+//  --------------------------------------------------241
+ // -----------------------------------------------1
+// let elem1 = document.querySelector('#elem1');
+// console.log(elem1);
+// let elem2 = document.querySelector('#elem2');
+// console.log(elem2);
+// let elem3 = document.querySelector('#elem3');
+// console.log(elem3);
+
+
+// ----------------------------------------2
+// let block = document.querySelector('#block p');
+// console.log(block);
+
+
+
+//  --------------------------------------------3
+//    let block = document.querySelector('.block p');
+//   console.log(block);
+
+
+
+// --------------------------------------------------4
+// let www = document.querySelector('.www');
+// console.log(www); // p
+
+
+
+
+// -----------------------------------------------------242
+
+
+// --------------------------------------------------------1
+// let button1 = document.querySelector('#button1');
+// let button2 = document.querySelector('#button2');
+// let button3 = document.querySelector('#button3');
+// button1.addEventListener('click', function() {
+// 	console.log('1');
+// });
+// button2.addEventListener('click', function() {
+// 	console.log('2');
+// });
+// button3.addEventListener('click', function() {
+// 	console.log('3');
+// });
+
+
+// ---------------------------------------------------------243
+
+// -------------------------------------------------------------1
+// let button1 = document.querySelector('#button1');
+// button1.addEventListener('click', function() {
+//      	console.log('func1');
+//     })
+//     let button2 = document.querySelector('#button2');
+// button1.addEventListener('click', function() {
+//      	console.log('func2');
+//     })
+
+
+// -----------------------------------------------------------------244
+// let elem1 = document.querySelector('#elem1');
+// let elem2 = document.querySelector('#elem2');
+// let elem3 = document.querySelector('#elem3');
+// let elem4 = document.querySelector('#elem4');
+// let elem5 = document.querySelector('#elem5');
+// elem1.addEventListener('click', func);
+// elem2.addEventListener('click', func);
+// elem3.addEventListener('click', func);
+// elem4.addEventListener('click', func);
+// elem5.addEventListener('click', func);
+// function func() {
+// 	console.log('message');
+// }
+
+
+//----------------------------------------------------------------------245
+// function func1() {
+// 	console.log('1');
+// }
+
+// function func2() {
+// 	console.log('2');
+// }
+
+// function func3() {
+// 	console.log('3');
+// }
+// let elem = document.querySelector('#elem');
+// elem.addEventListener('click', func1);
+// elem.addEventListener('click', func2);
+// elem.addEventListener('click', func3);
+
+// -----------------------------------------------------------------------------246
+
+// ------------------------------------------------------------------------------1
+// elem1.addEventListener('dblclick', function() {
+// 	console.log('Салам');
+// });
+
+// ---------------------------------------------------------------------------------2
+// elem1.addEventListener('mouseover', function() {
+// 	console.log('Салам');
+// });
+
+// -----------------------------------------------------------------------------------3
+// elem1.addEventListener('mouseout', function() {
+// 	console.log('Поставьте 5');
+// });
+
+// -----------------------------------------------------------------------------------4
+// elem1.addEventListener('mouseover', function() {
+// 	console.log('Привет');
+// });
+// elem1.addEventListener('mouseout', function() {
+// 	console.log('Пока');
+// });
